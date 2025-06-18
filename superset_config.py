e6a076a7aff1844b0064a6824dc5978742a169c7
Roman Ryabtsev <gjiuxarb@yandex.ru>
2025-06-18 13:31:11 +0300

    Изменение настроек в superset_config.py
    Изменение настроек в superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -3,20 +3,36 @@ from flask_appbuilder.security.manager import AUTH_LDAP
 from dotenv import load_dotenv
 import os
 
-# Загрузка переменных окружения из файла .env
+# ------------------ Загрузка переменных окружения ------------------
 load_dotenv()
 
-# Настройки подключения к базе данных
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('ENV_SUPERSET_DB_USER')}:{os.getenv('ENV_SUPERSET_DB_PASSWORD')}@{os.getenv('ENV_PG_HOST')}:{os.getenv('ENV_PG_PORT')}/{os.getenv('ENV_SUPERSET_DB_NAME')}"
+# ------------------ Подключение к базе данных ------------------
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://{os.getenv('ENV_SUPERSET_DB_USER')}:"
+    f"{os.getenv('ENV_SUPERSET_DB_PASSWORD')}@"
+    f"{os.getenv('ENV_PG_HOST')}:{os.getenv('ENV_PG_PORT')}/"
+    f"{os.getenv('ENV_SUPERSET_DB_NAME')}"
+)
 
-
-# Тайм-аут сессии
-SUPERSET_WEBSERVER_TIMEOUT = 60
-
-# Настройки CORS
+# ------------------ CORS ------------------
 ENABLE_CORS = True
 
-# Включение экспериментальных функций
+# ------------------ Безопасность ------------------
+WTF_CSRF_ENABLED = True
+WTF_CSRF_TIME_LIMIT = None
+
+# ------------------ Таймаут веб-сервера ------------------
+SUPERSET_WEBSERVER_TIMEOUT = 60
+
+# ------------------ Языковые настройки ------------------
+LANGUAGES = {
+    "ru": {"flag": "ru", "name": "Русский"},
+    "en": {"flag": "us", "name": "English"},
+}
+BABEL_DEFAULT_LOCALE = "ru"
+BABEL_DEFAULT_TIMEZONE = "Europe/Moscow"
+
+# ------------------ Экспериментальные фичи ------------------
 FEATURE_FLAGS = {
     "ALERT_REPORTS": True,
     "DASHBOARD_NATIVE_FILTERS": True,
